a68cbf1dd9e014e465d392d9a6c73b2c74b31f84
Bikram-Twana <50953026+Bikram-Twana@users.noreply.github.com>
2021-04-04 22:33:35 +0545

    Update quiz_brain.dart

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -45,7 +45,7 @@ class QuizBrain {
   }
 
   bool isFinished() {
-    if (_questionNumber >= _questionBank.length) {
+    if (_questionNumber >= _questionBank.length - 1) {
       print('Now returning true');
       return true;
     } else {
